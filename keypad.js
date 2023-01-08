0b2affbed4b6e95c4c74a32003db37a0c817493d
Sinjin Cooper <21377826+SinjinCooper@users.noreply.github.com>
2023-01-07 22:36:46 -0800

    Update keypad.js

--- keypad.js
+++ keypad.js
@@ -1,8 +1,9 @@
 
 const keypad_display = ["-", "-", "-", "-"]
 
+document.getElementById("display").innerHTML = keypad_display;
+
 function onPress(btn_num) {
-  window.alert("onpress");
   keypad_display.pop(btn_num);
   keypad_display.shift();
   document.getElementById("display").innerHTML = keypad_display.join("");
