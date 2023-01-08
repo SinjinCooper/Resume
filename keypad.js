8e44156c84cc017ca39132dadee52bf63f32eeca
Sinjin Cooper <21377826+SinjinCooper@users.noreply.github.com>
2023-01-07 22:31:09 -0800

    Update btn click logic

--- keypad.js
+++ keypad.js
@@ -1,6 +1,15 @@
-function onPress() {
-  window.alert("onpress")
+
+const keypad_display = ["-", "-", "-", "-"]
+
+function onPress(btn_num) {
+  window.alert("onpress");
+  keypad_display.pop(btn_num);
+  keypad_display.shift();
+  document.getElementById("display").innerHTML = keypad_display.join("");
 }
+
 function clearDisplay() {
   window.alert("clearDisplay")
+  keypad_display = ["-", "-", "-", "-"]
+  document.getElementById("display").innerHTML = keypad_display.join("");
 }
