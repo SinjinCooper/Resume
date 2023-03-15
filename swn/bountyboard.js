4c968e5aa73b3a3d46bc59fc127e0cd55a47c116
Sinjin Cooper <sinjincooper@Sinjins-MBP.lan1>
2023-03-14 23:50:16 -0700

    changed popup to table to show details

--- swn/bountyboard.js
+++ swn/bountyboard.js
@@ -10,7 +10,7 @@ var bounties_list = [
     ["Woogledoo", "POOLEMIT", "20,000 cr.", "I.D.G.A.F."],
     ["Torik Veld", "PIDWAAG", "12,500 cr.", "UNITED AEROSPACE DISTRIBUTORS"],
     ["Mr. John", "SVABODNAYA", "10,000 cr.", "ZEYRU CORP."],
-    ["Giovanni Giorgio", "9,060 cr.", "MORODER", "PAFT DUNK"],
+    ["Giovanni Giorgio", "MORODER", "9,060 cr.", "PAFT DUNK"],
     ["Moby Duke", "MOKRI", "8,000 cr.", "CAPTAIN REHAB"]
 ]
 
@@ -29,7 +29,7 @@ function showDetails(bounty) {
     document.getElementById("name").innerHTML = bounty[0];
     document.getElementById("crime").innerHTML = bounty[1];  
     
-    var stats =  document.getElementById("aa").style.display;
+    var stats =  document.getElementById("details_popup").style.display;
   
     if (stats == "none") {
 	    document.getElementById("details_popup").style.display = "inline-block";
