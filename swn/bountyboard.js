d8113adf3707304ce949041c85b831fea47a65e9
Sinjin Cooper <sinjincooper@Sinjins-MBP.lan1>
2023-03-14 23:48:01 -0700

    changed popup to table to show details

--- swn/bountyboard.js
+++ swn/bountyboard.js
@@ -10,23 +10,30 @@ var bounties_list = [
     ["Woogledoo", "POOLEMIT", "20,000 cr.", "I.D.G.A.F."],
     ["Torik Veld", "PIDWAAG", "12,500 cr.", "UNITED AEROSPACE DISTRIBUTORS"],
     ["Mr. John", "SVABODNAYA", "10,000 cr.", "ZEYRU CORP."],
-    ["Giovanni Giorgio", "9,060", "MORODER", "PAFT DUNK"],
-    ["Moby Duke", "MOKRI", "8,000", "CAPTAIN REHAB"]
+    ["Giovanni Giorgio", "9,060 cr.", "MORODER", "PAFT DUNK"],
+    ["Moby Duke", "MOKRI", "8,000 cr.", "CAPTAIN REHAB"]
 ]
 
-
-var table = document.getElementById("bounty_table");
-
 for (let i = 0; i < bounties_list.length; i++) {
+    var table = document.getElementById("bounty_table");
     var row = table.insertRow();
-    var cell0 = row.insertCell(0).innerHTML = bounties_list[i][0];
-    var cell1 = row.insertCell(1).innerHTML = bounties_list[i][1];
-    var cell2 = row.insertCell(2).innerHTML = bounties_list[i][2];
-    var cell3 = row.insertCell(3).innerHTML = bounties_list[i][3];
-  }
+    row.addEventListener("click", showDetails.bind(this, bounties_list[i]), false);
+
+    for (let j = 0; j < 4; j++) {
+        row.insertCell(j).innerHTML = bounties_list[i][j];
+    }
+}
 
 // Create custom pop up window with target details.
-function showDetails(x) {
-    x.innerHTML = a;
-    x.classList.toggle("show");
+function showDetails(bounty) {
+    document.getElementById("name").innerHTML = bounty[0];
+    document.getElementById("crime").innerHTML = bounty[1];  
+    
+    var stats =  document.getElementById("aa").style.display;
+  
+    if (stats == "none") {
+	    document.getElementById("details_popup").style.display = "inline-block";
+	} else {
+		document.getElementById("details_popup").style.display = "none";  
+	}
 }
