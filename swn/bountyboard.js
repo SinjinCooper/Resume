38cf7b60790c576b6ee5e07da0c495849a121ae6
Sinjin Cooper <sinjincooper@Sinjins-MBP.lan1>
2023-03-15 14:00:03 -0700

    update popup desc

--- swn/bountyboard.js
+++ swn/bountyboard.js
@@ -6,8 +6,9 @@ var a = "Eeek is wanted for various war crimes commited against the IDGAF. Serve
 //  -Target-  -Location-  -Reward-  -Requesting Party-  -Crime-  -Descr-
 var bounties_list = [
     ["Eeek", "MELGOR", "150,000 cr. A / 50,000 cr. D", "I.D.G.A.F.", "ALIVE/DEAD",
-        "Eeek, the War General for the Jin'opian Empire, is wanted for multiple war crimes committed against" +
-        "the IDGAF amongst the outer rim planets. His last known whereabouts are the Planet Melgor in the"],
+        `Eeek, the War General for the Jin'opian Empire, is wanted for multiple war crimes committed
+        agains the IDGAF amongst the outer rim planets. His last known whereabouts are the Planet
+        Melgor in the`],
     ["Robert Olmstead", "MOKRI", "10,000 cr. A", "OLMSTEAD ESTATE", "ALIVE", "Desc"],
     ["Woogledoo", "POOLEMIT", "20,000 cr. A / 15,000 cr. D", "I.D.G.A.F.", "ALIVE/DEAD", "Desc"],
     ["Torik Veld", "PIDWAAG", "12,500 cr. A / 6,000 cr. D", "UNITED AEROSPACE DISTRIBUTORS", "ALIVE/DEAD", "Desc"],
@@ -32,7 +33,7 @@ function showDetails(bounty) {
     document.getElementById("location").innerHTML = bounty[1];
     document.getElementById("reward").innerHTML = bounty[2];
     document.getElementById("wanted_status").innerHTML = bounty[4];  
-    document.getElementById("descr").innerHTML = bounty[6];  
+    document.getElementById("descr").innerHTML = bounty[5];  
     
     var stats =  document.getElementById("details_popup").style.display;
   
