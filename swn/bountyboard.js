562569a01b8cf7bc0be49487b3292a6eeccec04d
Sinjin Cooper <sinjincooper@Sinjins-MBP.lan1>
2023-03-15 14:34:41 -0700

    update popup desc

--- swn/bountyboard.js
+++ swn/bountyboard.js
@@ -1,19 +1,21 @@
-
-var a = `War General for the Jin'opian Empire. Wanted for multiple war crimes committed
-against the IDGAF among the outer rim planets. Last known whereabouts are the Planet
-Melgor in the`;
-
 // dynamically create table from bounties list
 //  -Target-  -Location-  -Reward-  -Requesting Party-  -Crime-  -Descr-
 var bounties_list = [
     ["Eeek", "MELGOR", "150,000 cr. A / 50,000 cr. D", "I.D.G.A.F.", "ALIVE/DEAD",
-        a],
+    `War General for the Jin'opian Empire. Wanted for multiple war crimes committed
+    against the IDGAF among the outer rim planets. Last known whereabouts are the Planet
+    Melgor in the`],
     ["Robert Olmstead", "MOKRI", "10,000 cr. A", "OLMSTEAD ESTATE", "ALIVE", "Desc"],
     ["Woogledoo", "POOLEMIT", "20,000 cr. A / 15,000 cr. D", "I.D.G.A.F.", "ALIVE/DEAD", "Desc"],
     ["Torik Veld", "PIDWAAG", "12,500 cr. A / 6,000 cr. D", "UNITED AEROSPACE DISTRIBUTORS", "ALIVE/DEAD", "Desc"],
     ["Mr. John", "SVABODNAYA", "10,000 cr. D", "ZEYRU CORP.", "DEAD", "Deasc"],
-    ["Giovanni Giorgio", "MORODER", "9,060 cr. A", "PAFT DUNK", "ALIVE", "Desc"],
-    ["Moby Duke", "MOKRI", "8,000 cr. D", "CAPTAIN REHAB", "DEAD", "Desc"]
+    ["Giovanni Giorgio", "MORODER", "9,060 cr. A", "PAFT DUNK", "ALIVE",
+        `His name is Giovanni Giorgio, but everyone just calls him Giorgio.`],
+    ["Moby Duke", "MOKRI", "8,000 cr. D", "CAPTAIN REHAB", "DEAD", "Desc"],
+    ["Me Lucky Charms", "", "reward cr.", "LUCKY THE LEPRECHAUN", "INTACT",
+        `Help! Oi cahn't foind me lahcky chahrms! Oi balieve it's taht dryshite Trix Rabbut be
+        stealin' me goods! Oi'll pay ye well for me ayght chahrms bahck, with a little extrae for
+        tee gobshite rabbut's heed. Aijajiyjajiy!`]
 ];
 
 for (let i = 0; i < bounties_list.length; i++) {
@@ -33,7 +35,6 @@ function showDetails(bounty) {
     document.getElementById("reward").innerHTML = bounty[2];
     document.getElementById("wanted_status").innerHTML = bounty[4];  
     document.getElementById("descr").innerHTML = bounty[5];
-    window.alert(bounty[5]);
     
     var stats =  document.getElementById("details_popup").style.display;
   
